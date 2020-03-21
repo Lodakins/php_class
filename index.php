46df447c37f3558c3a8439acba2df472bc832752
Akinrolabu Lolade <busayoololade@yahoo.com>
2020-03-21 17:25:33 +0100

    first commit added all files

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title> Contact Form</title>
+</head>
+<body>
+
+<form action="submitform.php" method="POST">
+<legend> Please all (*) fiels are compulsory </legend>
+<p><label for="firstname"> First Name</label>
+<input type="text" name="firstname" id="firstname" >
+</p>
+<p>
+<label for="lastname"> Last Name</label>
+<input type="text" name="lastname" id="lastname" >
+</p>
+<p>
+<label for="surname"> Surname</label>
+<input type="text" name="surname" id="surname" >
+</p>
+
+
+</form>
+
+
+
+
+</body>
+</html>
